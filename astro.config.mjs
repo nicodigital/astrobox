a529e68b25096681fb32f91fa3ebb48807f68897
Nicolás González <nicolook.pro@gmail.com>
2024-01-09 17:35:29 -0300

    Start New Branch
    Comenzando pruebas con sistema Hybrido

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'hybrid',
   site: 'https://nicowebsite.com/astro',
   adapter: netlify()
 });
