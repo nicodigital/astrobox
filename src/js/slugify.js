2e643382411ee4fa0b04f3ae73fab05591ea359c
Nicolás González <nicolook.pro@gmail.com>
2024-01-25 14:42:09 -0300

    Improve Slugify

--- src/js/slugify.js
+++ src/js/slugify.js
@@ -1,12 +1,16 @@
 export function slugify(text) {
-  return text
+  const normalizedText = text
     .toString()
     .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]+/g, '')
-    .replace(/--+/g, '-')
-    .replace(/^-+/, '')
-    .replace(/-+$/, '');
+    .normalize("NFD") // Normalizar caracteres especiales
+    .replace(/[\u0300-\u036f]/g, ""); // Eliminar diacríticos
+
+  return normalizedText
+    .replace(/\s+/g, '-') // Reemplazar espacios con guiones
+    .replace(/[^\w-]+/g, '') // Eliminar caracteres especiales
+    .replace(/--+/g, '-') // Reemplazar múltiples guiones con uno solo
+    .replace(/^-+/, '') // Eliminar guiones al principio
+    .replace(/-+$/, ''); // Eliminar guiones al final
 }
 
-export default slugify
+export default slugify;
