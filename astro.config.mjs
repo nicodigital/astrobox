a112bed7d8c516a6f39b467d0a9ee56d10ba1501
Nicolás González <nicolook.pro@gmail.com>
2024-01-25 14:28:27 -0300

    Films Archive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output:'hybrid', 
-  site: 'https://nicowebsite.com/astro',
-  adapter: netlify()
+  integrations: [tailwind(), icon()],
+  output: 'hybrid',
+  // site: 'https://nicowebsite.com/astro',
+  adapter: netlify(),
+  prefetch: true
 });
