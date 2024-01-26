25564fe04211a1e123263939fbf5436e6e2be885
Nicolás González <nicolook.pro@gmail.com>
2024-01-26 18:24:13 -0300

    Set Astro Server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: 'hybrid',
+  output: 'server',
   // site: 'http://astro.test/astrobox/dist',
   adapter: netlify(),
   prefetch: true,
