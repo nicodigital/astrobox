53283cd5a269acafb231aaf6a43fa816016e68f1
Nicolás González <nicolook.pro@gmail.com>
2024-02-18 12:20:58 -0300

    Add Smooth Scroll

--- .netlify/functions-internal/ssr/chunks/prerender_jZbd4QN7.mjs
+++
@@ -1,6 +0,0 @@
-const noop = () => {};
-export const _ = noop;
-export const a = noop;
-export const b = noop;
-export const g = noop;
-export const i = noop;
--- src/js/globals.js
+++ src/js/globals.js
@@ -297,3 +297,5 @@ if (animations) {
 	});
 
 }
+
+// import "../js/smoothscroll";
