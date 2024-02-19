614e1c14b65ce3480133b8e71e65e230877f8097
Nicolás González <nicolook.pro@gmail.com>
2024-02-19 08:44:42 -0300

    Add HTMX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import htmx from 'astro-htmx';
 import netlify from "@astrojs/netlify";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: 'hybrid',
+  integrations: [tailwind(), htmx(), icon()],
+  output: 'server', // <-- Esto es condicional para HTMX
 
   // Site y Base son para colocar un Build en producción - En este caso también configurar HOME_URL en .env
   // Aqui va el dominio
