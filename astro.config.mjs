3d60b15b15b2fa9b871357d362334b83938c3ee2
Nicolás González <nicolook.pro@gmail.com>
2024-09-04 06:47:12 -0300

    Add Deploy Bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
+import tunnel from 'astro-tunnel';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), tunnel()],
   output: 'static',
 
   // Site y Base son para colocar un Build en producción - En este caso también configurar HOME_URL en .env
